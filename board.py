d26fa3e97ded5ef0567c66d1495999deb4d0fedb
raacee <raceldamechli@outlook.fr>
2025-07-18 16:07:30 +0200

    game works

--- board.py
+++ board.py
@@ -2,27 +2,19 @@ import numpy as np
 from collections import deque
 from player import Player
 
+BLACK_STONE=1
+WHITE_STONE=-1
+EMPTY=0
+PASS=[]
 
 class Board:
-    BLACK_STONE=1
-    WHITE_STONE=-1
-    PASS = []
-    EMPTY=0
 
     def __init__(self, board_size:int, super_ko:bool = False):
         self._board_size: int = board_size
-        self._board: np.ndarray = np.array([[Board.EMPTY] * self._board_size] * self._board_size)
+        self._board: np.ndarray = np.array([[EMPTY] * self._board_size] * self._board_size)
         self._super_ko: bool = super_ko
-        if self._super_ko:
-            self._last_boards = [
-                self._board[::,::]
-            ]
-        else:
-            self._last_boards = (
-                self._board[::,::]
-                ,
-            )
-        self._players: deque[Player] = deque([Player(stone=Board.BLACK_STONE), Player(stone=Board.WHITE_STONE)])
+        self._last_boards: list[np.ndarray] = []
+        self._players: deque[Player] = deque([Player(stone=BLACK_STONE), Player(stone=WHITE_STONE)])
         self._last_turn_induced_capture: bool = False
         self._captured_black_stones: int = 0
 
@@ -30,23 +22,21 @@ class Board:
         return self._board[x,y] != 0
 
     def _check_ko(self, x, y, board) -> bool:
-        if self._super_ko:
-            return board in self._last_boards
-        else:
-            return board == self._last_boards[0]
+        return board in self._last_boards
 
     def game(self):
         last_turned_passed = False
         while True:
             # Begin new player turn
             current_player: Player = self._players[0]
+            print(f"{self._board}")
             print(f"Player {current_player}'s turn")
             print("Place a stone on a valid case")
 
             # Players chooses a stone
             player_choice = current_player.choose_case()
             # Check if player has chosen to pass
-            if player_choice == Board.PASS:
+            if player_choice == PASS:
                 # If player chose to pass check if last turned was passed
                 if last_turned_passed:
                     # If it did, break and initiate counting
@@ -61,6 +51,10 @@ class Board:
 
             x, y = player_choice
 
+            if x >= self._board_size or y >= self._board_size:
+                print("Coordinates bigger than board_size")
+                continue
+
             # TODO: check for ko
             # At each turn, save turn and check if next turn leads to saved turn
 
@@ -71,34 +65,41 @@ class Board:
 
             # Check if case induces suicide
             test_board = self._board[::,::]
-            self._board[x, y] = current_player.stone
+            test_board[x, y] = current_player.stone
             induces_suicide = self._induces_suicide(x, y, current_player.stone, test_board)
-            if induces_suicide:
-                print("Placing a stone here induces suicide. Forbidden move.")
-                self._board[x, y] = Board.EMPTY
-                continue
-            else:
-                self._board = test_board
 
             # Check if case induces capture
+            induces_capture = False
             neighbors = self._neighbors_indices(x, y)
             # already_checked_stones_indices = set()
             for x_n, y_n in neighbors:
-                neighbor_stone = self._board[x_n, y_n]
+                neighbor_stone = test_board[x_n, y_n]
                 if neighbor_stone == current_player.stone * -1:
                     group_indices_tuples = self._flood_fill(x_n, y_n, neighbor_stone)
                     n_stones = len(group_indices_tuples)
                     group_stones = [0] * n_stones
                     for i, index in enumerate(group_indices_tuples):
-                        group_stones[i] = self._board[index]
+                        group_stones[i] = test_board[index]
                     if 0 not in group_stones:
+                        induces_capture = True
                         print(f"Player {current_player.stone} captures group")
-                        if neighbor_stone == Board.BLACK_STONE:
+                        if neighbor_stone == BLACK_STONE:
                             self._captured_black_stones += n_stones
                         for i, index in enumerate(group_indices_tuples):
-                            self._board[index] = 0
+                            test_board[index] = 0
                         # already_checked_stones_indices.update(group_indices_tuples)
 
+            if induces_suicide:
+                if not induces_capture:
+                    print("Player case choice induces suicide with no capture, illegal move")
+                    continue
+            else:
+                self._board = test_board
+
+            if self._super_ko:
+                self._last_boards.append(self._board)
+            else:
+                self._last_boards = [self._board]
             self._players.rotate()
 
         print("Game has ended")
@@ -118,7 +119,7 @@ class Board:
         while len(queue) > 0:
             x, y = queue.popleft()
             if (x, y) not in res:
-                if self._board[x, y] == Board.EMPTY:
+                if self._board[x, y] == EMPTY:
                     res.add((x,y))
                     continue
                 elif self._board[x, y] == stone:
@@ -144,7 +145,7 @@ class Board:
         while len(queue) > 0:
             x, y = queue.popleft()
             if (x, y) not in checked:
-                if board_array[x, y] == Board.EMPTY:
+                if board_array[x, y] == EMPTY:
                     checked.add((x,y))
                     return False
                 elif board_array[x, y] == stone:
@@ -167,8 +168,8 @@ class Board:
 
     def _winner(self) -> int:
         board = self._board[::,::]
-        black_stones = np.sum((board == Board.BLACK_STONE).astype(int)) + self._captured_black_stones
+        black_stones = np.sum((board == BLACK_STONE).astype(int)) + self._captured_black_stones
         return -1 if black_stones > 180 else 1
 
     def __repr__(self):
-        return self._board.__repr__()
+        return self.___repr__()
--- player.py
+++ player.py
@@ -1,12 +1,11 @@
-from board import Board
+import board
 
 
 class Player:
     def __init__(self, stone: int) -> None:
-        if stone not in [Board.WHITE_STONE, Board.BLACK_STONE]:
+        if stone not in [board.WHITE_STONE, board.BLACK_STONE]:
             raise ValueError("Stone must be black (-1) or white (1)")
         self.stone = stone
-        self.captured_stones = 0
 
     def choose_case(self) -> tuple[int, ...]:
         return tuple(int(v) for v in input().split()[:2])
