61eb18448cbf6621354816becec23e5daf797c98
raacee <raceldamechli@outlook.fr>
2025-10-29 14:13:38 +0100

    reformats

--- game/player.py
+++ game/player.py
@@ -1,27 +1,32 @@
 from abc import ABC, abstractmethod
 from .stones import WHITE_STONE, BLACK_STONE
 
+
 class Player(ABC):
     def __init__(self, stone: int) -> None:
         if stone not in [WHITE_STONE, BLACK_STONE]:
             raise ValueError("Stone must be black (-1) or white (1)")
-        self.stone = stone
+        self.stone: int = stone
         self.name = "White" if stone == WHITE_STONE else "Black"
 
     @abstractmethod
-    def choose_case(self, board=None) -> tuple[int, ...]:
+    def choose_case(self, board) -> tuple[int, int]:
         raise NotImplementedError()
 
+
 class Human(Player):
-    def choose_case(self, board=None) -> tuple[int, ...]:
-        return tuple(int(v) for v in input("Enter your move (row and column): ").split()[:2])
+    def choose_case(self, board) -> tuple[int, int]:
+        return tuple(
+            int(v) for v in input("Enter your move (row and column): ").split()[:2]
+        )
+
 
 class Bot(Player):
     def __init__(self, stone: int) -> None:
         super().__init__(stone)
 
-    def choose_case(self, board=None) -> tuple[int, ...]:
+    def choose_case(self, board) -> tuple[int, int]:
         return self._find_best_move(board)
 
-    def _find_best_move(self, board) -> tuple[int, ...]:
+    def _find_best_move(self, board) -> tuple[int, int]:
         raise NotImplementedError()
